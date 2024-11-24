9faf0cf121ec31720b0b5fae3421610eb91724d6
arrthur <86019472+arrtthur@users.noreply.github.com>
2024-11-24 06:44:59 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,12 @@
 function expandPost(postElement) {
-    // Esconde todas as outras notas
-    document.querySelectorAll('.post .content').forEach(content => {
+    document.querySelectorAll('.content').forEach(content => {
         if (content !== postElement.querySelector('.content')) {
             content.style.display = 'none';
         }
     });
 
-    // Alterna a exibição do conteúdo da nota clicada
     const content = postElement.querySelector('.content');
-    if (content.style.display === 'none' || content.style.display === '') {
-        content.style.display = 'block';
-    } else {
-        content.style.display = 'none';
-    }
+    content.style.display = content.style.display === 'none' || !content.style.display
+        ? 'block'
+        : 'none';
 }
